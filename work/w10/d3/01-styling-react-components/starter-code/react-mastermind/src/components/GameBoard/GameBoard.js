78118253e175a452702d57fa442eefdcbea6bd8c
jim-clark <email@jim-clark.com>
2018-02-14 04:51:41 -0800

    Add styling-react-components lesson

---
+++ work/w10/d3/01-styling-react-components/starter-code/react-mastermind/src/components/GameBoard/GameBoard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import GuessRow from '../GuessRow/GuessRow';
+
+const GameBoard = (props) => {
+  return (
+    <div>
+      {props.guesses.map((guess, idx) => 
+        <GuessRow
+          guess={guess}
+          colors={props.colors}
+          rowIdx={idx}
+          currentGuess={idx === (props.guesses.length - 1)}
+          key={idx}
+        />
+      )}
+    </div>
+  );
+}
+
+export default GameBoard;
---
+++ work/w10/d3/01-styling-react-components/starter-code/react-mastermind/src/components/GuessRow/GuessRow.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import GuessPegs from '../GuessPegs/GuessPegs';
+import GuessScore from '../GuessScore/GuessScore';
+import ScoreButton from '../ScoreButton/ScoreButton';
+
+const GuessRow = (props) => {
+  return (
+    <div>
+      <div>{props.rowIdx + 1}</div>
+      <GuessPegs code={props.guess.code} colors={props.colors} />
+      {
+        props.currentGuess ?
+          <ScoreButton /> :
+          <GuessScore score={props.guess.score} />
+      }
+    </div>
+  );
+}
+
+export default GuessRow;
